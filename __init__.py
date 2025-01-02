b4dd55ae5f7d6ac980ffcd1677567ae808db71cb
Zachary Westerman <westerman.zachary@gmail.com>
2025-01-01 20:49:59 -0600

    Add documentation

--- __init__.py
+++ __init__.py
@@ -10,12 +10,34 @@ from pathlib import Path
 
 @functools.cache
 def get_query_text(name: str) -> str:
+    """
+    Reads the content of a text file located in the 'queries' directory and returns it as a string.
+
+    Args:
+        name (str): The name of the text file (without the .txt extension) to read.
+
+    Returns:
+        str: The content of the specified text file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        IOError: If there is an error reading the file.
+    """
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(f'{dir_path}/queries/{name}.txt') as fp:
         return fp.read()
 
 
 def snake_case_to_camel_case(snake_str: str) -> str:
+    """
+    Convert a snake_case string to camelCase.
+
+    Args:
+        snake_str (str): The snake_case string to be converted.
+
+    Returns:
+        str: The converted camelCase string.
+    """
     if not snake_str:
         return ''
 
@@ -24,6 +46,15 @@ def snake_case_to_camel_case(snake_str: str) -> str:
 
 
 def camel_case_to_snake_case(camel_str: str) -> str:
+    """
+    Converts a camelCase string to snake_case.
+
+    Args:
+        camel_str (str): The camelCase string to be converted.
+
+    Returns:
+        str: The converted snake_case string.
+    """
     if not camel_str:
         return ''
 
@@ -31,10 +62,25 @@ def camel_case_to_snake_case(camel_str: str) -> str:
 
 
 class SessionError(Exception):
+    """
+    Exception raised for errors related to session handling.
+    """
     pass
 
 
 class InvalidQueryError(SessionError):
+    """
+    Exception raised for invalid query names.
+
+    This error is raised when a query name provided by the user does not match
+    any of the available query names. It suggests the closest matches to the
+    invalid query name.
+
+    Methods:
+        __init__(name: str): Initializes the InvalidQueryError with the invalid
+                             query name and suggests the closest matches.
+    """
+
     def __init__(self, name: str):
         # Get the closest matches to the query name,
         # and suggest them to the user.
@@ -56,17 +102,63 @@ class InvalidQueryError(SessionError):
 
 
 class ServerError(SessionError):
+    """
+    Exception raised for server errors during API requests.
+    This error means that something went wrong on the server side,
+    or the server could not fulfill the request (due to insufficient permissions, etc.).
+
+    Methods:
+        __init__(code: int): Initializes the ServerError with the status code
+                             of the failed API request
+    """
+
     def __init__(self, code: int):
         super().__init__(f'API request failed with status code {code}')
 
 
 class ClientError(SessionError):
+    """
+    Exception raised for client errors during API requests.
+    This error means that the client's request is invalid, and the server cannot process it.
+
+    Methods:
+        __init__(message: str): Initializes the ClientError with the error message.
+    """
+
     def __init__(self, message: str):
         super().__init__(f'Error in API request: {message}')
 
 
 class Session:
+    """
+    A class to manage Skrunk API sessions and make API calls.
+
+    Attributes:
+        api_key (str): The API key used for authentication.
+        url (str): The base URL of the API. Defaults to 'localhost'.
+        port (int | None): The port to use for the connection. Defaults to 5000 for 'localhost' and 443 otherwise.
+
+    Methods:
+        __init__(api_key: str, url: str = 'localhost', port: int | None = None):
+            Initializes the Session with the given API key, URL, and port.
+
+        call(query_name: str, variables: dict | None = None) -> str:
+            Makes a POST request to the API with the given query and variables.
+
+        __getattr__(name: str) -> Callable:
+            Dynamically handles attribute access to allow API calls by calling a method on the Session object.
+    """
+
     def __init__(self, api_key: str, url: str = 'localhost', *, port: int | None = None):
+        """
+        Initialize the API client.
+
+        Args:
+            api_key (str): The API key for authentication.
+            url (str, optional): The base URL of the API. Defaults to 'localhost'.
+            port (int, optional): The port to connect to. Defaults to 5000 if url is 'localhost', otherwise 443.
+        """
+
         if port is None:
             port = 5000 if url == 'localhost' else 443
 
@@ -74,6 +166,22 @@ class Session:
         self.api_key = api_key
 
     def call(self, query_name: str, variables: dict | None = None) -> str:
+        """
+        Executes a GraphQL query by sending a POST request to the API.
+
+        Args:
+            query_name (str): The name of the query to be executed.
+            variables (dict | None, optional): A dictionary of variables to be passed with the query. Defaults to None.
+
+        Returns:
+            str: The data returned from the API for the given query.
+
+        Raises:
+            InvalidQueryError: If the query file is not found.
+            ClientError: If the server returns a 500 status code.
+            ServerError: If the server returns a status code outside the range of 200-299.
+        """
+
         query = snake_case_to_camel_case(query_name)
 
         try:
